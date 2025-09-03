4e0a0275d19b6b40c94828130811a22d378b86a3
Miguel Lozano <miguelozano913@gmail.com>
2025-09-03 15:52:23 -0500

    agregue la funcion de un gif de fondo

--- js/main.js
+++ js/main.js
@@ -10,7 +10,13 @@ function estrella(){
     figura.style.clipPath = " polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)";
 }
 
+function gif() {
+    figura.style.backgroundImage = "url(img/1rRk.gif)";
+    figura.style.backgroundSize = "cover";
+}
+
 document.addEventListener('DOMContentLoaded', function(){
     document.getElementById('btnCircle').addEventListener('click', circulo);
     document.getElementById('btnStar').addEventListener('click', estrella);
+    document.getElementById('btnGif').addEventListener('click', gif);
 })
