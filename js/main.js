02a664252882dfb7e6a83a0d1f46f3e8601d96b3
Miguel Lozano <miguelozano913@gmail.com>
2025-09-03 15:44:17 -0500

    agregue la funcion de cambiar a estrella

--- js/main.js
+++ js/main.js
@@ -6,6 +6,11 @@ function circulo() {
     figura.style.background = "black";
 }
 
+function estrella(){
+    figura.style.clipPath = " polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)";
+}
+
 document.addEventListener('DOMContentLoaded', function(){
     document.getElementById('btnCircle').addEventListener('click', circulo);
+    document.getElementById('btnStar').addEventListener('click', estrella);
 })
